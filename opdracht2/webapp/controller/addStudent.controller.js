887609f6f40f6831358670aa2f17365e8eef129f
MichielVerhaegen <michiel.verhaegen@student.hogent.be>
2023-08-17 16:56:00 +0000

    create fixed

--- opdracht2/webapp/controller/addStudent.controller.js
+++ opdracht2/webapp/controller/addStudent.controller.js
@@ -13,20 +13,16 @@ sap.ui.define([
       onInit: function () {
         console.log('in init');
         this.oOwnerComponent = this.getOwnerComponent();
-        console.log('naowner');
         this.oRouter = this.oOwnerComponent.getRouter();
-        console.log('narouter');
         this.oRouter.attachRouteMatched(this._onRouteMatched, this);
-        console.log('naroutematched');
-
       },
       HandleSave: function () {
         var oForm = this.getView().getModel("form").getData();
-        console.log("oForm", oForm);
+        console.log("oForm",oForm);
         var oView = this.getView();
         var oDataModel = oView.getModel();
         console.log(oDataModel);
-        oDataModel.create("/studentSet", oForm, {
+        oDataModel.create("/studentsSet", oForm, {
           success: function (data, response) {
             MessageBox.success("Data was created successfully");
           },
@@ -40,15 +36,14 @@ sap.ui.define([
 
       },
       _onRouteMatched: function (oEvent) {
+        console.log('inroutmatch');
         var oView = this.getView();
         var oDataModel = oView.getModel();
-      
-        console.log('inonroutmatched');
 
         var oStudent = {
-          FORENAME: "",
-          LASTNAME: "",
-          CLASS: ""
+          Forename: "",
+          Lastname: "",
+          Class: ""
         };
         console.log('vooromodel');
         var oModel = new JSONModel(oStudent);
